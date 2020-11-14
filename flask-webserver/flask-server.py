de3d1d6adcf34f77bf9a6f7d52106f3c0b1caeb3
TomD53 <tomtomgaming101@gmail.com>
2020-11-14 00:48:47 +0000

    Added javascript to check for changes to the status of the card and then show / hide the card accordingly.
    Ditched websockets since I couldn't get them working how i wanted, something to make it more efficient in future
    
    just need to add hotkey binding for the toggle_stats_card function and need to write code to actually feed it stats

--- flask-webserver/flask-server.py
+++ flask-webserver/flask-server.py
@@ -4,6 +4,7 @@ from gevent.pywsgi import WSGIServer
 app = Flask(__name__)
 
 app.template_folder = "render_templates"
+app.static_folder = "static"
 
 
 class Stat:
@@ -14,7 +15,8 @@ class Stat:
         self.kit = kit
 
 
-stats = [Stat("Most Kills", "65", "b0squet", "archer")]
+stats = [Stat("Most Kills", "65", "b0squet", "archer"),
+         Stat("Most Deaths", "40", "TomD53", "medic")]
 
 
 @app.route("/")
@@ -22,16 +24,27 @@ def hello_world():
     return "Welcome to the CMS stream tool web server"
 
 
+
 @app.route("/statscard/")
 def stats_page():
-    return "The stats page"
+    return render_template("stats_card_no_content.html")
 
 
 overlay_status_dict = {
     "stats_card": {
-        "status": "showing"  # can be "hidden" or "showing"
+        "status": "hidden"  # can be "hidden" or "showing"
     }
 }
+
+
+def toggle_stats_card():
+    if overlay_status_dict["stats_card"]["status"] == "showing":
+        overlay_status_dict["stats_card"]["status"] = "hidden"
+        print("Hiding stats card")
+    elif overlay_status_dict["stats_card"]["status"] == "hidden":
+        overlay_status_dict["stats_card"]["status"] = "showing"
+        print("Showing stats card")
+
 
 
 @app.route("/overlay_status", methods=["GET"])
@@ -44,11 +57,12 @@ def stats_content():
     return render_template("stats_card_content.html", stats_for_card=stats, map_info="Map 1 - Adrenaline I")
 
 
+
+
 if __name__ == '__main__':
     # Debug/Development
-    app.run(host="localhost", port=5000, debug=False)
+    #app.run(host="localhost", port=5000, debug=False)
 
-    """
     # Production
     http_server = WSGIServer(('', 5000), app, log=None)
     http_server.serve_forever()
@@ -57,8 +71,11 @@ if __name__ == '__main__':
 
     log = logging.getLogger('werkzeug')
     log.setLevel(logging.ERROR)
+
+
+
     
-    """
+
 
 
 
